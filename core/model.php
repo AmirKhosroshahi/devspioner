f216283a717281c11dd3d0749dc6c2a681c2aad1
Amir Khosroshahi <barnamenvican@gmail.com>
2023-06-29 19:37:13 +0330

    change env base url

--- core/model.php
+++ core/model.php
@@ -8,9 +8,9 @@ class Model
     {
        
         $serverName = 'localhost';
-        $userName = 'admin_devspioner';
-        $password = 'devspioner_89089';
-        $dbName = 'admin_devspioner';
+        $userName = 'root';
+        $password = '';
+        $dbName = 'mysite';
         self::$conn = new PDO('mysql:host=' . $serverName . ';dbname=' . $dbName, $userName, $password);
         self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         header("Access-Control-Allow-Origin: http://212.90.103.135:3000/");
